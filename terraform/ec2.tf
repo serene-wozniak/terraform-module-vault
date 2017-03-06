88aa42673ead3ae095b10fd4b81df0b007bd5c89
Miles Wilson <miles.wilson@opencredo.com>
2017-03-06 11:47:05 +0000

    [MSP-19] use route53 internally

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -35,6 +35,7 @@ module "vault_bootstrap" {
     vault_postgres_password = "${var.vault_password}"
     vault_postgres_endpoint = "${aws_db_instance.vault_db.endpoint}"
     vault_postgres_sslmode  = "disable"
+    consul_join_node        = "${ var.ha ? "vault.${var.route53_domain}" : "vault-01.${var.route53_domain}" }"
   }
 
   ssh_ca_publickey      = "${var.ssh_user_ca_publickey}"
